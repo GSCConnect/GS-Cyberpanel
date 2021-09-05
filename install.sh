95bd40ab760f59317a18a305996f18dbfc16ee5b
GSCConnect <82615884+GSCConnect@users.noreply.github.com>
2021-09-04 19:13:02 -0500

    Playing around

--- install.sh
+++ install.sh
@@ -2,30 +2,32 @@
 
 OUTPUT=$(cat /etc/*release)
 if  echo $OUTPUT | grep -q "CentOS Linux 7" ; then
-        echo "Checking and installing curl and wget"
+        echo "Checking and installing curl and wget bro"
 yum install curl wget -y 1> /dev/null
 yum update curl wget ca-certificates -y 1> /dev/null
                 SERVER_OS="CentOS"
 elif echo $OUTPUT | grep -q "CentOS Linux 8" ; then
-        echo -e "\nDetecting Centos 8...\n"
+        echo -e "\nDetecting Centos 8...Looser \n"
         SERVER_OS="CentOS8"
+        echo -e "Yo thats gay"
 yum install curl wget -y 1> /dev/null
 yum update curl wget ca-certificates -y 1> /dev/null
 elif echo $OUTPUT | grep -q "CloudLinux 7" ; then
-        echo "Checking and installing curl and wget"
+        echo "Checking and installing curl and wget brooo"
 yum install curl wget -y 1> /dev/null
 yum update curl wget ca-certificates -y 1> /dev/null
                 SERVER_OS="CloudLinux"
 elif echo $OUTPUT | grep -q "Ubuntu 18.04" ; then
 apt install -y -qq wget curl
                 SERVER_OS="Ubuntu"
-elif echo $OUTPUT | grep -q "Ubuntu 20.04" ; then
+elif echo $OUTPUT | grep -q "Ubuntu 20.04 | The OG Nigga" ; then
 apt install -y -qq wget curl
                 SERVER_OS="Ubuntu"
+                echo -e "My nigga"
 else
 
-                echo -e "\nUnable to detect your OS...\n"
-                echo -e "\nCyberPanel is supported on Ubuntu 18.04, CentOS 7.x and CloudLinux 7.x...\n"
+                echo -e "\nUnable to detect your OS... You windows Faggot\n"
+                echo -e "\nYou gotta have some Ubuntu 18.04, CentOS 7.x and CloudLinux 7.x...\n"
                 exit 1
 fi
 
